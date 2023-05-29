e139edfd884e83be4d48d56d93ee5ee1fff7853e
Martin Fedorenko <MartinFedorenkoLK1288@gmail.com>
2023-05-28 22:02:52 -0300

    correcciones entrega 1 y pruebas entrega 2

---
+++ src/CargadorDatos.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import  java.io.PrintWriter;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.io.FileNotFoundException;
+
+public class CargadorDatos {
+    List<String[]> dataLines = new ArrayList<>();
+
+
+
+    public String convertToCSV(String[] data) {
+
+        return Stream.of(data)
+                .map(this::escapeSpecialCharacters)
+                .collect(Collectors.joining(","));
+    }
+
+    public void givenDataArray_whenConvertToCSV_thenOutputCreated() throws IOException {
+        File csvOutputFile = new File("src\\datos.csv");
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            dataLines.stream()
+                    .map(this::convertToCSV)
+                    .forEach(pw::println);
+        }
+
+    }
+
+    public String escapeSpecialCharacters(String data) {
+        String escapedData = data.replaceAll("\\R", " ");
+        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            data = data.replace("\"", "\"\"");
+            escapedData = "\"" + data + "\"";
+        }
+        return escapedData;
+    }
+
+
+
+}
---
+++ src/Validador.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Validador {
+    private static final String PASSWORD_REGEX =
+            "^(?=.*\\d)(?!.* {2})(?=.*[a-z])(?=.*[A-Z]).{8,64}$";
+    //de 8 a 64 caracteres, sin espacios consecutivos, al menos una minuscula, una mayuscula y un digito
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile(PASSWORD_REGEX);
+
+    //valida si password aparece entre las 10000 peores contraseñas
+    public boolean validate_weak_password(String password)
+    {
+        try {
+            File file = new File("src\\top-10000-passwords.txt");
+            Scanner file_reader = new Scanner(file);
+            while (file_reader.hasNextLine()) {
+                String weak_password = file_reader.nextLine();
+                if(password.equalsIgnoreCase(weak_password))
+                    return false;
+            }
+            file_reader.close();
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    //valida si password cumple con la estructura especificada
+    public boolean validate_password_regex(String password)
+    {
+        return (PASSWORD_PATTERN.matcher(password).matches());
+    }
+
+    public boolean validate_password(String password){
+
+        if(validate_password_regex(password) && validate_weak_password(password)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
