3b03e554149ac31be36a04acb1b46e2dbc08c41a
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-07-17 16:46:48 -0300

    Incidentes

--- src/main/Incidente.java
+++ src/main/Incidente.java
@@ -4,20 +4,26 @@ import java.util.ArrayList;
 import java.time.LocalDate;
 
 //Después deberíamos importarlo de Person.java, Prestacion.java y de Comunidad.java
-public class Persona{
+public class Persona {
 
 }
 
-public class Prestacion{
+public class Prestacion {
 
 }
 
-public class Comunidad{
+public class Comunidad {
 
+}
+
+public class RepoIncidentes {
+    List<Incidente> listaIncidentes;
+
+    //public Incidente buscarIncidente(){}
 }
 
 public class Incidente {
-    private static String id;
+    private String id;
     Persona personaQueReporto;
     Prestacion prestacionIncidentada;
     public String descripcion;
@@ -27,16 +33,35 @@ public class Incidente {
     boolean estado;
     List<Persona> suscriptores;
 
-    public static void crearIncidente(){
+    RepoIncidentes repoIncidentes;
 
+    //1. Se debe permitir la apertura de incidentes
+    public Incidente(String id, Persona persona, Prestacion prestacion, String descripcion, List<Comunidad> comunidadesAfectadas, List<Persona> suscriptores, RepoIncidentes repoIncidentes;){
+        this.id = id;
+        this.personaQueReporto = persona;
+        this.descripcion = descripcion;
+        this.comunidadesAfectadas = comunidadesAfectadas;
+        this.apertura = LocalDate.now();
+        this.cierre = null;
+        this.estado = true;
+        this.suscriptores = suscriptores;
+
+        this.repoIncidentes = repoIncidentes;
     }
-
+    //2. Se debe permitir el cierre de incidentes
     public void cerrarIncidente(){
         this.cierre = LocalDate.now();
         this.estado = false;        
     }
 
     public void main(){
-        //Incidente unIncidente = new Incidente("id1", new Persona(), new Prestacion(), "Descripción.", [new Comunidad()], LocalDate.now().minusDays(1), null, true, [new Persona()]);
+        Incidente unIncidente = new Incidente("id1", new Persona(), new Prestacion(), "Descripción.", [new Comunidad()], [new Persona()]);
+    }
+}
+
+public class GestorDeRankings {
+    RepoIncidentes repoIncidentes;
+    public crearRanking(){
+
     }
 }
