970fd28ba8c0016093d1b41af74ca82861d4a43e
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-09-20 20:07:29 -0300

    Avances
    Co-authored-by: nahuelcolotto <nahuelcolotto@users.noreply.github.com>

--- calculoDeRanking/src/main/java/Grupo11/calculoDeRanking/calculoDeRanking/calculoDeRankingController.java
+++ calculoDeRanking/src/main/java/Grupo11/calculoDeRanking/calculoDeRanking/calculoDeRankingController.java
@@ -2,8 +2,9 @@ package Grupo11.calculoDeRanking.calculoDeRanking;
 
 import Grupo11.calculoDeRanking.Entidades.Entidad;
 import Grupo11.calculoDeRanking.Incidentes.Incidente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,7 +12,10 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 //import Grupo11.calculoDeRanking.Comunidades.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static Grupo11.calculoDeRanking.calculoDeRanking.calculoDeRanking.ordenarEntidades;
 
 @RestController
 public class calculoDeRankingController {
@@ -30,6 +34,40 @@ public class calculoDeRankingController {
     public List<Entidad> getListaEntidades() {
         return calculoDeRanking.generarListaEntidades();
     }
+
+    @PostMapping("/calcularRanking")
+    public List<Entidad> calcularEntidad(@RequestBody List<Entidad> entidades){
+        //Da error cuando se invoca al método ordenarEntidades()
+        return ordenarEntidades(entidades, 1);
+    }
+
+    @PostMapping("/validarEntidadesPrueba")
+    public Integer validar(@RequestBody List<Entidad> entidades){
+        return 201;
+    }
+
+    @GetMapping("/generarJson")
+    public List<Entidad> generarJson() {
+        List<Entidad> entidades = new ArrayList<>();
+        Entidad entidad = new Entidad();
+        entidades.add(entidad);
+        Incidente incidente = new Incidente(1L, "Observación de ejemplo.", null, null, null, null, null);
+        List<Incidente> incidentes = new ArrayList<>();
+        incidentes.add(incidente);
+        incidentes.add(incidente);
+        entidad.setIncidentes_reportados(incidentes);
+        entidades.add(entidad);
+        return entidades;
+    }
+    @GetMapping("/generarJsonI")
+    public List<Incidente> generarJsonI(){
+        List<Incidente> incidentes = new ArrayList<>();
+        Incidente incidente = new Incidente(1L, "Observación de ejemplo.", null, null, null, null, null);
+        incidentes.add(incidente);
+        return incidentes;
+    }
+
+
 
     @GetMapping("/pruebaInsertar")
     public int insertarPrueba(){
