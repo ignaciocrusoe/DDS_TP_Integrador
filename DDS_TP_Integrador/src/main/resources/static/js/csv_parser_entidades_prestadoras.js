93993ed9d9d8d9c48ebc19ce425471582dea8b86
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-12-17 18:56:58 -0300

    Importar entidades

--- DDS_TP_Integrador/src/main/resources/static/js/csv_parser_entidades_prestadoras.js
+++ DDS_TP_Integrador/src/main/resources/static/js/csv_parser_entidades_prestadoras.js
@@ -1,29 +1,82 @@
+(function () {
+  var DELIMITER = ';';
+  var NEWLINE = '\n';
+  var qRegex = /^"|"$/g;
+  var button = document.getElementById('boton-cargar-archivo');
+  var i = document.getElementById('csv-file');
 
-document.getElementById("boton-cargar-archivo").addEventListener("click", function() {
-  leer_csv_entidades();
-});
+  var iterator = 0;
 
-function leer_csv_entidades() {
-  var listOfObjects = [
-    { categoria: 'Supermercado', nombre_entidad: 'Cotito', organismo_de_control: 1, prestador: 1},
-    { categoria: 'Supermercado', nombre_entidad: 'Cototo', organismo_de_control: 1, prestador: 1}
-  ];
+  if (!i) {
+      return;
+  }
 
-  const firstNombreEntidad = listOfObjects[0].nombre_entidad;
-        console.log(firstNombreEntidad);
+  button.addEventListener('click', function () {
+      if (!!i.files && i.files.length > 0) {
+          parseCSV(i.files[0]);
+      }
+  });
 
-  fetch('/importar-entidades-prestadoras/csv', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(listOfObjects),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
+  function parseCSV(file) {
+      if (!file || !FileReader) {
+          return;
+      }
+
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          toTable(e.target.result);
+      };
+
+      reader.readAsText(file);
+  }
+
+  function toTable(text) {
+      if (!text || !table) {
+          return;
+      }
+
+      // clear table
+      while (!!table.lastElementChild) {
+          table.removeChild(table.lastElementChild);
+      }
+
+      var rows = text.split(NEWLINE);
+      var headers = rows.shift().trim().split(DELIMITER);
+
+      headers.forEach(function (h) {
+
+          var ht = h.trim();
+
+          if (!ht) {
+              return;
+          }
+
+
+      });
+
+      rows.forEach(function (r) {
+          r = r.trim();
+
+          if (!r) {
+              return;
+          }
+
+          var cols = r.split(DELIMITER);
+
+          if (cols.length === 0) {
+              return;
+          }
+
+
+          cols.forEach(function (c) {
+              var tc = c.trim();
+              iterator = iterator + 1;
+              //en tc está el elemento
+              console.log("tc = ", tc);
+
+          });
+
+      });
+  }
+})();
