55917f09f483d8198a40f734fd2b3bfd0ca0cfdf
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-12-18 23:11:38 -0300

    Guardar datos CSV

---
+++ DDS_TP_Integrador/src/main/java/Grupo11/DDS_TP_Integrador/Repositories/OrganismoControlRepository.java
@@ -0,0 +1,12 @@
+package Grupo11.DDS_TP_Integrador.Repositories;
+
+import Grupo11.DDS_TP_Integrador.Entidades.OrganismoControl;
+import Grupo11.DDS_TP_Integrador.Notificadores.Notificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+
+public interface OrganismoControlRepository extends JpaRepository<OrganismoControl,Long> {
+}
