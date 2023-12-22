761c4b04983dcc43a9f9412a2ae481ad0e861e81
Martin Fedorenko <67556470+Martin-Fedorenko@users.noreply.github.com>
2023-12-22 16:40:15 -0300

    ahhh subite

--- DDS_TP_Integrador/src/main/java/Grupo11/DDS_TP_Integrador/Requests/AbandonarComunidadRequest.java
+++ DDS_TP_Integrador/src/main/java/Grupo11/DDS_TP_Integrador/Requests/AbandonarComunidadRequest.java
@@ -5,4 +5,11 @@ import lombok.Data;
 public class AbandonarComunidadRequest {
     private Long idMiembroAEliminar;
 
+    public Long getIdMiembroAEliminar() {
+        return idMiembroAEliminar;
+    }
+
+    public void setIdMiembroAEliminar(Long idMiembroAEliminar) {
+        this.idMiembroAEliminar = idMiembroAEliminar;
+    }
 }
